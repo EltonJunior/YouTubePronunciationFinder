2b36e2e430b15a4c3345cf4d07e2d2e590c564b6
Elton Junior <elton.junior@continental.com>
2024-10-21 15:21:16 -0400

    Initial release
    This initial release, the program is working.

---
+++ YouTubePronunciationFinder/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace YouTubePronunciationFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.panelBrowser = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(413, 12);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(105, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtWord
+            // 
+            this.txtWord.Location = new System.Drawing.Point(175, 14);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(232, 20);
+            this.txtWord.TabIndex = 1;
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(524, 39);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(363, 316);
+            this.lstResults.TabIndex = 2;
+            this.lstResults.DoubleClick += new System.EventHandler(this.lstResults_DoubleClick);
+            // 
+            // panelBrowser
+            // 
+            this.panelBrowser.Location = new System.Drawing.Point(12, 39);
+            this.panelBrowser.Name = "panelBrowser";
+            this.panelBrowser.Size = new System.Drawing.Size(506, 322);
+            this.panelBrowser.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "How do I pronounce the word ->";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 377);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelBrowser);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.btnSearch);
+            this.MaximumSize = new System.Drawing.Size(919, 416);
+            this.MinimumSize = new System.Drawing.Size(919, 416);
+            this.Name = "Form1";
+            this.Text = "YouTubePronunciationFinder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Panel panelBrowser;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ YouTubePronunciationFinder/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace YouTubePronunciationFinder
+{
+
+    public partial class Form1 : Form
+    {
+        private ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize CefSharp
+            Cef.Initialize(new CefSettings());
+
+            // Create a new browser instance
+            browser = new ChromiumWebBrowser("about:blank");
+            browser.Dock = DockStyle.Fill;
+
+            // Add the browser to the panel
+            panelBrowser.Controls.Add(browser);
+        }
+
+        private async Task SearchYouTube(string query)
+        {
+            string apiKey = "YOUR_API_KEY"; // Replace with your API key
+            string url = $"https://www.googleapis.com/youtube/v3/search?part=snippet&q={query} pronunciation&type=video&key={apiKey}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetStringAsync(url);
+                var json = JObject.Parse(response);
+
+                lstResults.Items.Clear();
+
+                foreach (var item in json["items"])
+                {
+                    string title = item["snippet"]["title"].ToString();
+                    string videoId = item["id"]["videoId"].ToString();
+                    lstResults.Items.Add(new { Title = title, VideoId = videoId });
+                }
+            }
+        }
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string word = txtWord.Text;
+            await SearchYouTube(word);
+        }
+
+        private void lstResults_DoubleClick(object sender, EventArgs e)
+        {
+            //if (lstResults.SelectedItem != null)
+            //{
+            //    dynamic selected = lstResults.SelectedItem;
+            //    string videoId = selected.VideoId;
+            //    // webBrowser.Navigate($"https://www.youtube.com/watch?v={videoId}");
+            //    System.Diagnostics.Process.Start($"https://www.youtube.com/watch?v={videoId}");
+
+            //}
+            if (lstResults.SelectedItem != null)
+            {
+                dynamic selected = lstResults.SelectedItem;
+                string videoId = selected.VideoId;
+                string url = $"https://www.youtube.com/embed/{videoId}?autoplay=1";
+
+                // Load the video in the embedded browser
+                browser.Load(url);
+                // Ensure the browser takes the full size of the panel
+                browser.Dock = DockStyle.Fill;
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+            base.OnFormClosing(e);
+        }
+
+    }
+}
